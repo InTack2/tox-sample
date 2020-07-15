09fed39973d5dd2c410702082e76ffe0675b1b0b
Tack2 <takumi236@gmail.com>
2020-07-16 01:12:56 +0900

    cfgに伴いsetup内容変更

--- setup.py
+++ setup.py
@@ -1,22 +1,13 @@
 # !/usr/bin/env python
 from setuptools import setup
-from setuptools import find_packages
 
 install_requires = [
     "requests",
 ]
 
 
-def _requires_from_file(filename):
-    return open(filename).read().splitlines()
-
-
-setup(name='tox_sample',
-      version='0.1',
-      description='tox_sample',
-      author='tox_sample',
-      author_email='sample@sample.sample',
-      url='',
-      packages=find_packages("src.tox_sample"),
-      install_requires=install_requires
-      )
+setup(
+    name='tox_sample',
+    version='0.1',
+    install_requires=install_requires
+)

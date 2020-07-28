fc391f5679937e5d36a386e167eb8b36511206b6
Tack2 <takumi236@gmail.com>
2020-07-28 22:58:27 +0900

    mayaの各バージョンを追加。conftestにmayaのinitializeの処理を追加

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,3 +1,23 @@
 import sys
 import os
+
+import pytest
+
 sys.path.append(os.path.abspath(os.path.dirname(os.path.abspath(__file__)) + "/../src/"))
+
+
+__maya_test = bool(os.environ.get("MAYA_TEST"))
+maya_flag = False
+
+
+@pytest.fixture(scope="session", autouse=__maya_test)
+def check_maya_test():
+    import maya.cmds as cmds
+    import maya.standalone
+    maya.standalone.initialize("python")
+    info = cmds.about(installedVersion=True)
+    # info = cmds.about(version=True)
+    version = info.split(" ")
+    print(version)
+    yield
+    maya.standalone.uninitialize()
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-"""code test pour src\main.py.
+"""code test pour src/main.py.
 """
 from __future__ import print_function
 from __future__ import unicode_literals
@@ -19,19 +19,3 @@ class TestDoubleTotalizer(object):
     def test_sam(self):
         result = DoubleTotalizer().sam(1, 2)
         assert result == 6
-
-
-def test_maya():
-    try:
-        import maya.cmds as cmds
-        import maya.standalone
-        maya.standalone.initialize("python")
-        info = cmds.about(installedVersion=True)
-        # info = cmds.about(version=True)
-        version = info.split(" ")
-        print(version)
-        maya.standalone.uninitialize()
-    except:
-        pass
-    else:
-        assert True
